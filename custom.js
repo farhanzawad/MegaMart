b4517041dd3a2956dd9f4f56320f7e6d8c18a84d
Ahmed Farhan <bsmrmu23007.zawad@bsmrmu.edu.bd>
2023-11-19 10:02:42 +0600

    Initials added & three sections

---
+++ custom.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#008ECC',
+        heading: '#666666',
+        text: '#222222',
+        background: '#F3F9FB',
+        background2: '#F5F5F5',
+      }
+      
+    }
+  }
+};
